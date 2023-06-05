c310224c75a01fc25e0c7a1d2f1e8709211457c4
Internate <87392271+Nathan8044@users.noreply.github.com>
2023-06-05 16:31:56 +0000

    interface_changes_update

--- static/styles/index.js
+++ static/styles/index.js
@@ -30,7 +30,32 @@ selectBox.addEventListener('change', function() {
 //  end of interface_changes access and trunk port options 
 
 
-// start of interface_changes speed policy options
+var option1 = document.getElementById('yes_speed_policy');
+var option2 = document.getElementById('no_speed_policy');
+var content1 = document.getElementById('content1');
 
 
-//end of interface_changes speed policy options
+option1.addEventListener('click', function() {
+  content1.classList.remove('hidden');
+
+});
+
+option2.addEventListener('click', function() {
+  content1.classList.add('hidden');
+
+});
+
+var option1 = document.getElementById('yes_ip_add');
+var option2 = document.getElementById('no_ip_add');
+var contentYesIp = document.getElementById('content_yes_ip');
+
+
+option1.addEventListener('click', function() {
+  contentYesIp.classList.remove('hidden');
+
+});
+
+option2.addEventListener('click', function() {
+  contentYesIp.classList.add('hidden');
+
+});
