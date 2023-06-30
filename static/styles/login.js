d059c04f24c9ad3db193a562e0d48b36f2ef1b48
Internate <87392271+Nathan8044@users.noreply.github.com>
2023-06-30 12:42:34 +0000

    Version 0.24

--- static/styles/login.js
+++ static/styles/login.js
@@ -4,3 +4,14 @@ function login_config () {
     localStorage.setItem('username', user_name)
     localStorage.setItem('password', pass_word)
 }
+function validateForm() {
+    var userNameInput = document.getElementById('user_name');
+    var passwordInput = document.getElementById('pass_word');
+    
+    if (userNameInput.value === '' || passwordInput.value === '') {
+        alert('Please enter both username and password.');
+    } else {
+        login_config();
+        window.location.href = "/home"; // Redirect to the desired page
+    }
+}
